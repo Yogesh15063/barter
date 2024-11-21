d6af1a7269c79db56b432696caa0d4000accf4d1
Yogesh15063 <yj15063@gmail.com>
2024-11-21 18:31:06 +0530

    initial commit

---
+++ barter/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100 ">
+      <div>
+        <img className="h-7" src="assets/paw.png" alt="" />
+      </div>
+      <div>
+        <ul>
+            <li>
+                POST
+            </li>
+            <li>
+                BARTER FOR
+            </li>
+            <li>
+                CCONTACT
+            </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
